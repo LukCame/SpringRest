f29aa69b1d135fab944049485290188894a7afdd
Luca <l.camerlengo>
2017-07-04 14:37:47 +0200

    firstCommit

---
+++ src/main/java/it/dstech/model/Studente.java
@@ -0,0 +1,41 @@
+package it.dstech.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Studente {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+		
+	private int eta;
+	
+	private String nome;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getEta() {
+		return eta;
+	}
+
+	public void setEta(int eta) {
+		this.eta = eta;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
